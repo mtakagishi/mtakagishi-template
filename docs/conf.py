e9fae73fe471330fd0ed4d7d4f44d69837e35284
mtakagishi <mtakagishi+github@gimail.com>
2023-04-30 22:07:24 +0900

    設定を整備

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'mtakagishi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+    'sphinx.ext.todo',
+    'sphinxcontrib.blockdiag',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +27,27 @@ language = 'jp'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+
+html_title = project
+html_theme = "pydata_sphinx_theme"
+html_logo = "_static/logo.png"
+
+html_theme_options = {
+    "logo": {
+        "text": project,
+    },
+}
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+[extensions]
+todo_include_todos = True
+
+# blockdiag
+blockdiag_html_image_format = 'SVG'
+blockdiag_fontpath = 'docs/_static/ipaexg.ttf'
